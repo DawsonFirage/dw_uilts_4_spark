e3a93a1ffb69d8cfa836888842663c1e30995a2e
daoxinzhang <1014262538@qq.com>
2023-09-22 10:47:26 +0800

    feat:update spark-thriftserver.sh

--- Scripts/spark-thriftserver/spark-thriftserver-monitor.sh
+++ Scripts/spark-thriftserver/spark-thriftserver-monitor.sh
@@ -4,7 +4,11 @@
 # 服务地址 192.168.54.7
 # 服务端口号 10086
 
-# 创建 flag 标志，确定服务是否需要重启
+# 查看当前文件路径，主要是为了确定spark-thriftserver.sh的位置。如果两个脚本不在同一位置，则需要手动指定该值。
+FILE_PATH="$(dirname "$(readlink -f "$0")")"
+echo "file_path=${FILE_PATH}"
+# 设置thrift_server的端口号
+THRIFT_SERVER_PORT="10086"
 
 # 查看Hive Metastore是否开启，若Hive Matastore不在运行则退出程序
 ps -aux | grep hive | grep metastore > /dev/null
@@ -15,19 +19,19 @@ fi
 echo "Hive Matastore正在运行中..."
 
 #查看 Thrift Server 端口是否开启
-netstat -nltp | grep 10086  > /dev/null
+netstat -nltp | grep ${THRIFT_SERVER_PORT}  > /dev/null
 if [ $? -ne 0 ]; then
     echo "Thrift Server端口异常！"
-    ps -aux | grep 10086 | grep spark-thriftserver
+    ps -aux | grep ${THRIFT_SERVER_PORT} | grep spark-thriftserver
     if [ $? -ne 0 ]; then
         echo "Thrift Server服务未开启！"
         echo "正在开启Thrift Server服务..."
-        sh /home/admin/script/spark-thriftserver.sh start
+        sh ${FILE_PATH}/spark-thriftserver.sh start
     else
         echo "Thrift Server服务异常！"
         echo "正在重启Thrift Server服务..."
-        sh /home/admin/script/spark-thriftserver.sh stop
+        sh ${FILE_PATH}/spark-thriftserver.sh stop
         sleep 2s
-        sh /home/admin/script/spark-thriftserver.sh start
+        sh ${FILE_PATH}/spark-thriftserver.sh start
     fi
 fi
--- Scripts/spark-thriftserver/spark-thriftserver.sh
+++ Scripts/spark-thriftserver/spark-thriftserver.sh
@@ -10,12 +10,24 @@
 
 # 生效对应的环境变量
 export ADMIN_PATH=/home/admin
-#source ${ADMIN_PATH}/bin/common/common.sh
-
+source "${ADMIN_PATH}/.bashrc"
+#source "${ADMIN_PATH}/${DW_DB}/bin/common/common.sh"
 
 # 需要预先配置环境变量
 # SPARK_HOME=
-source /home/admin/.bashrc
+echo "SPARK_HOME=${SPARK_HOME}"
+# 设置hive metastore
+HIVE_METASTORE_URIS="thrift://mdw:9083"
+# 设置Spark thrift-server的host和port
+#如果要更改thrift-server的port，记得同步修改spark-thriftserver-monitor.sh中的port。
+THRIFT_SERVER_HOST="mdw"
+THRIFT_SERVER_PORT="10086"
+# 设置thrift-server资源
+DRIVER_MEMORY="2G"
+EXECUTOR_CORES="2"
+EXECUTOR_MEMORY="4G"
+NUM_EXECUTORS="3"
+DRIVER_MAX_RESULT_SIZE="1G"
 
 arg=$1
 # 启动 Thrift Server
@@ -31,22 +43,22 @@ if [ "${arg}" == "start" ];then
     echo "正在启动Thrift Server..."
 
     # 删除历史log数据
-    rm -rf ${SPARK_HOME}/log/spark-admin-org.apache.spark.sql.hive.thriftserver.HiveThriftServer2-1-mdw.out*
+    rm -rf ${SPARK_HOME}/logs/spark-admin-org.apache.spark.sql.hive.thriftserver.HiveThriftServer2-1-mdw.out*
 
     # 启动 ThriftServer
     ${SPARK_HOME}/sbin/start-thriftserver.sh \
     --master yarn \
     --deploy-mode client \
     --name spark-thriftserver \
-    --driver-memory 2G \
-    --executor-cores 4 \
-    --executor-memory 4G \
-    --num-executors 3 \
-    --conf "spark.driver.maxResultSize=4G" \
+    --driver-memory ${DRIVER_MEMORY} \
+    --executor-cores ${EXECUTOR_CORES} \
+    --executor-memory ${EXECUTOR_MEMORY} \
+    --num-executors ${NUM_EXECUTORS} \
+    --conf "spark.driver.maxResultSize=${DRIVER_MAX_RESULT_SIZE}" \
     --conf "spark.hadoop.mapreduce.fileoutputcommitter.marksuccessfuljobs=false" \
-    --hiveconf "hive.metastore.uris=thrift://mdw:9083" \
-    --hiveconf "hive.server2.thrift.port=10086" \
-    --hiveconf "hive.server2.thrift.bind.host=mdw" \
+    --hiveconf "hive.metastore.uris=${HIVE_METASTORE_URIS}" \
+    --hiveconf "hive.server2.thrift.bind.host=${THRIFT_SERVER_HOST}" \
+    --hiveconf "hive.server2.thrift.port=${THRIFT_SERVER_PORT}" \
     --hiveconf "hive.exec.stagingdir=/tmp/hive/.hive-staging" \
     --hiveconf "hive.insert.into.multilevel.dirs=true" \
 #    --jars ${JOB_HUTOOL_PATH},${JOB_HTTPCORE_PATH},${JOB_HTTPCLIENT_PATH},${JOB_HTTPMIME_PATH}
@@ -57,32 +69,32 @@ if [ "${arg}" == "start" ];then
         echo "等待Thrift Server启动中，当前已等待`expr ${i} \* 10`秒。"
         sleep 10s
         #查看 Thrift Server 端口是否开启
-        netstat -nltp | grep 10086 > /dev/null
+        netstat -nltp | grep ${THRIFT_SERVER_PORT} > /dev/null
         if [ $? -eq 0 ]; then
             break
         fi
     done
 
-    ps -aux | grep 10086 | grep spark-thriftserver
+    ps -aux | grep ${THRIFT_SERVER_PORT} | grep spark-thriftserver
     if [ $? -eq 0 ]; then
         echo "Thrift Server启动成功！"
     else
         echo "[ERROR] Thrift Server启动失败，请查看日志！！"
-        cat ${SPARK_HOME}/log/spark-admin-org.apache.spark.sql.hive.thriftserver.HiveThriftServer2-1-mdw.out
+        cat ${SPARK_HOME}/logs/spark-admin-org.apache.spark.sql.hive.thriftserver.HiveThriftServer2-1-mdw.out
     fi
     
 # 关闭 Thrift Server
 elif [ "${arg}" == "stop" ];then
-    ps -aux | grep 10086 | grep spark-thriftserver
+    ps -aux | grep ${THRIFT_SERVER_PORT} | grep spark-thriftserver
     if [ $? -eq 0 ]; then
-        ps -aux | grep 10086 | grep spark-thriftserver | gawk '{print $2}' | xargs -n1 kill -15
+        ps -aux | grep ${THRIFT_SERVER_PORT} | grep spark-thriftserver | gawk '{print $2}' | xargs -n1 kill -15
     else
         echo "[ERROR] 未发现Thrift Server进程！"
     fi
 
     # kill 后等待程序关闭，端口释放
     sleep 5s
-    ps -aux | grep 10086 | grep spark-thriftserver
+    ps -aux | grep ${THRIFT_SERVER_PORT} | grep spark-thriftserver
     if [ $? -eq 0 ]; then
         echo "Thrift Server关闭成功！"
     else
